0e83f2a9aa51767ad726ab102945bd4154234f54
asel0211 <61904820+asel0211@users.noreply.github.com>
2020-03-08 10:06:31 +1100

    Add files via upload

---
+++ Compensable Billing Monte Carlo Simulation.R
@@ -0,0 +1,47 @@
+### Compensable Billing Model
+
+# growth: Variation in presentation growth rate
+# per.comp: Variation in percentage compensable 
+# price.patient: Variation in $ per patient
+# per.rec: Variation in % recoverable
+# cost.patient: Variation in cost per patient
+
+# an.pres: Annual presentations
+# an.comp: Annual compensable
+# price.pot: $ potentially recoverable
+# price.act: $ actually recoverable
+# cost: Cost of recovery in wages
+# total: Total $ recovered
+
+results2 = NULL
+for (k in 1:100)
+{
+  an.pres <- 91828
+  an.comp <- 3287
+  
+      results = NULL
+      for (k in 1:30)
+        {
+        growth <- rpert(1, min=1, mode=1.0274, max=1.0548, shape=4)
+        per.comp <- rpert(1, min=0.0322, mode=0.0358, max=0.0394, shape=4)
+        price.patient <- rpert(1, min=19.68, mode=21.87, max=24.06, shape=4)
+        per.rec <- rpert(1, min=0.80, mode=0.90, max=1, shape=4)
+        cost.patient <- rpert(1, min=0.0233, mode=0.0260, max=0.0285, shape=4)
+        
+        an.pres <- an.pres*growth
+        an.comp <- an.pres*per.comp
+        price.pot <- an.comp*price.patient
+        price.act <- price.pot*per.rec
+        cost <- an.comp*cost.patient
+        total <- price.act - cost
+        
+        results = rbind(results, data.frame(total))
+        }
+    
+    npv <- NPV(cf0=69756.69, cf=results$total, times=1:30, i=0.02, plot=FALSE)
+            # npv function requires FinancialMath package
+    
+    results2 = rbind(results2, data.frame(npv))
+                }
+
+summary(results2)
