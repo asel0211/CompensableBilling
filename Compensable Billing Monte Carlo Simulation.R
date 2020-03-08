9cd231860d6c924918fa5e920c3615b9f9fc35a7
asel0211 <61904820+asel0211@users.noreply.github.com>
2020-03-08 20:14:58 +1100

    Add files via upload

--- Compensable Billing Monte Carlo Simulation.R
+++ Compensable Billing Monte Carlo Simulation.R
@@ -14,17 +14,18 @@
 # total: Total $ recovered
 
 results2 = NULL
-for (k in 1:50000)
+for (k in 1:50000) # Number of iterations
 {
   an.pres <- 91828
   an.comp <- 3287
   
       results = NULL
-      for (k in 1:30)
+      for (k in 1:30) # Simulates 30 years of data
         {
         growth <- rpert(1, min=1, mode=1.0274, max=1.0548, shape=4)
         per.comp <- rpert(1, min=0.0322, mode=0.0358, max=0.0394, shape=4)
         price.patient <- rpert(1, min=19.68, mode=21.87, max=24.06, shape=4)
+        #price.patent <- rpert(1, min 60.14, mode=66.82, max=73.50, shape=4)  
         per.rec <- rpert(1, min=0.80, mode=0.90, max=1, shape=4)
         cost.patient <- rpert(1, min=0.0233, mode=0.0260, max=0.0285, shape=4)
         
@@ -44,6 +45,8 @@ for (k in 1:50000)
     results2 = rbind(results2, data.frame(npv))
                 }
 
-summary(results2)
-quantile(results2, probs = c(0.025, 0.975), 1)
-ggplot(results2, aes(x=npv)) + geom_histogram(binwidth = 10000)
+results2 %>% summary()
+results2 %>% quantile(probs = c(0.025, 0.975), 1)
+results2 %>% ggplot(aes(x=npv,
+                     fill=I("light blue"), 
+                     col=I("black"))) + geom_histogram(binwidth = 10000)
